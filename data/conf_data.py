710ac0f98dfa743a90860067a497f7b49f94694d
billy-ship-it <npeyrouset@gmail.com>
2023-12-05 11:58:00 +0000

    changed files wikipedia

---
+++ data/conf_data.py
@@ -0,0 +1,9 @@
+# choix des charactères que l'on garde dans les données récupérées
+
+characters_to_remove = [
+    '=', '.', ',', '(', ')', '0', '1', '2', '3', '4', '5', '6',
+    '7', '8', '9', '«', '»', '-', '_', 'ⓐ', '\\', ';', '/', '%'
+    ':', '?']
+
+traduction_table = str.maketrans('', '', ''.join(characters_to_remove))
+
--- data/save_data.py
+++ data/save_data.py
@@ -1,5 +1,7 @@
 import requests
 import pickle
+import conf_data
+import sys
 
 # URLs de l'API de Wikipedia dans différentes langues
 dic_api = {
@@ -14,7 +16,6 @@ dic_api = {
             "pl": "https://pl.wikipedia.org/w/api.php",
             "sv": "https://sv.wikipedia.org/w/api.php"
             }
-
 
 for language in dic_api.keys():
     file = open(
@@ -32,12 +33,16 @@ for language in dic_api.keys():
         if response.status_code == 200:
             data = response.json()
             page_content = list(data["query"]["pages"].values())[0]["extract"]
+            page_content = [word for word in page_content.split()]
+            data = [chaine.translate(conf_data.traduction_table).lower() for chaine in page_content]
+            data = [''.join([c for c in word if ord(c)<255]) for word in data]
+            data = list(filter(lambda x: x != '', data))
         else:
             print("La requête a échoué. Statut :", response.status_code)
         try:  # error if there is a backslash in the request
             with open(
                 f"data/{language}/{request}.txt", "w", encoding='utf-8'
                     ) as file2:
-                file2.write(page_content)
+                file2.write(str(data))
         except FileNotFoundError:
             print(f"problem for the request: {request}")
