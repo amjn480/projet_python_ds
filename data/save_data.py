988016fc2204640f82cb7167c325b34827b2053d
billy-ship-it <npeyrouset@gmail.com>
2023-12-12 20:28:00 +0000

    add scrapping data in file data

--- data/save_data.py
+++ data/save_data.py
@@ -4,9 +4,10 @@ import json
 import conf_data
 
 def main():
-    for language in ['fr']:
+
+    for language in conf_data.dic_api.keys():
         file = open(
-            f"/home/onyxia/work/projet_python_ds/list_of_wikiarticle/list_{language}.pickle",
+            f"/home/onyxia/work/projet_python_ds/data/list_of_wikiarticle/list_{language}.pickle",
             "rb")
         requests_list = pickle.load(file)
         for request in requests_list:
---
+++ data/scrapping_data.py
@@ -0,0 +1,26 @@
+import requests
+import pickle
+import os
+import conf
+
+S = requests.Session()
+
+language_list = ["en", "pl", "fr", "es", "it", "de", "ca", "sv", "af", "nl"]
+
+for language in language_list:
+    URL = f"https://{language}.wikipedia.org/w/api.php"
+    data = []
+    for letter in range(ord("A"), ord("Z")+1):
+        PARAMS = {
+            "action": "opensearch",
+            "namespace": "0",
+            "search": chr(letter),
+            "limit": "15",
+            "format": "json"
+        }
+
+        response = S.get(url=URL, params=PARAMS)
+        data += response.json()[1]
+    file = open(f"list_of_wikiarticle/list_{language}.pickle", "wb")
+    pickle.dump(data, file)
+    file.close()
