c70266bc5f0f2ad757d0141e16acca611e905454
billy-ship-it <npeyrouset@gmail.com>
2023-12-12 10:06:31 +0000

    file code_markov tested

--- code_markov.py
+++ code_markov.py
@@ -3,40 +3,46 @@ import os
 
 code_space = 0
 
+
 def encoding(c):
-    '''This function encodes an ASCII character'''
-    return None
+    '''This function encodes an ASCII character that'''
+    if ord(c) <= 122:
+        return ord(c) - 97
+    elif 224 <= ord(c):
+        return ord(c) - (198)
+    elif c == 'space':
+        return 58
 
 
 class Matrix():
-    def __init__(self, number_char, language):
+    def __init__(self, language):
         self.language = language
-        self.nb = number_char
-        self.matrix = np.zeros((number_char, number_char))
-        self.total = np.zeros(number_char)
-        self.list_articles = os.listdir(f"data/{language}")
+        self.matrix = np.zeros((255, 255))
+        self.total = np.zeros(255)
+        self.list_articles = os.listdir(f"/home/onyxia/work/projet_python_ds/data/{language}")
+        self.train()
+        self.normalize()
 
-    def update_matrix(self, language, file):
-        with open(f"data/{language}/{file}.txt", 'r') as data:
+    def update_matrix(self, file):
+        with open(f"/home/onyxia/work/projet_python_ds/data/{self.language}/{file}", 'r') as data:
             for word in data:
                 for k in range(len(word)-1):
                     character = word[k]
                     next_character = word[k+1]
-                    i = encoding(character)
-                    j = encoding(next_character)
+                    i = ord(character)
+                    j = ord(next_character)
                     self.matrix[i][j] += 1
                     self.total[i] += 1
-                self.matrix[encoding[word[-1]], code_space] += 1
+                self.matrix[ord(word[-1])][32] += 1
 
     def train(self):
-        for file in self.list_articles:
-            self.update_matrix(language=language, file=file)
+        for file in self.list_articles[:10]:
+            self.update_matrix(file=file)
+    
+    def normalize(self):
+        for k in range(255):
+            if self.total[k] != 0:
+                self.matrix[k] = self.matrix[k]/self.total[k]
 
-
-        #il faudra diviser les probas par total pour avoir bien une matrice de transition et voir comment on calcule les epsilon
-
-
-language_list = ["en", "pl", "fr", "es", "it", "de", "ca", "sv", "af", "nl"]
-list_matrix=[]
-for language in language_list :
-    list_matrix.append(proba(language, number_char))
+# Pour visualiser la matrice
+# np.savetxt('matrix', Matrix(language='fr').matrix)
