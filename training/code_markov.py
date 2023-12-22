31b912774345ecf68bd8ebf50818380b76ddb717
billy-ship-it <npeyrouset@gmail.com>
2023-12-22 12:39:35 +0000

    add class frequencies

--- training/code_markov.py
+++ training/code_markov.py
@@ -44,7 +44,7 @@ class Matrix():
                 print(f"error collecting the data :{file}")
 
     def train(self):
-        for file in self.list_articles[:10]:
+        for file in self.list_articles:
             self.update_matrix(file=file)
     
     def normalize(self):
@@ -52,21 +52,29 @@ class Matrix():
             if self.total[k] != 0:
                 self.matrix[k] = self.matrix[k]/self.total[k]
 
-    def compute_proba(self, text):
-        """text is a list of word"""
-        proba = 1
-        for word in text:
-            proba *= self.matrix[32][ord(word[0])]
-            for k in range(len(word)-1):
-                character = word[k]
-                next_character = word[k+1]
-                i = ord(character)
-                j = ord(next_character)
-                proba *= self.matrix[i][j]
-            proba *= self.matrix[ord(word[-1])][32]
-        return proba
+
+# # Pour visualiser la matrice
+# for language in conf_training.dic_api.keys():
+#     np.savetxt(f"/home/onyxia/work/projet_python_ds/training/Matrix/Matrix_{language}.txt", Matrix(language=language).matrix)
 
 
-# Pour visualiser la matrice
-for language in conf_training.dic_api.keys():
-    np.savetxt(f"/home/onyxia/work/projet_python_ds/training/Matrix/Matrix_{language}.txt", Matrix(language=language).matrix)
+class Frequency():
+    def __init__(self, language):
+        self.language = language
+        self.frequencies = np.zeros(255)
+        self.list_articles = os.listdir(f"/home/onyxia/work/projet_python_ds/data/{language}")
+        self.train()
+    
+    def train(self):
+        number_characters = 0
+        for file in self.list_articles:
+            with open(f"/home/onyxia/work/projet_python_ds/data/{self.language}/{file}", 'r', encoding='utf-8') as file2:
+                try:
+                    data = json.load(file2)
+                    for word in data:
+                        number_characters += len(word)
+                        for letter in word:
+                            self.frequencies[ord(letter)] += 1
+                except json.decoder.JSONDecodeError:
+                    print(f"error collecting the data :{file}")
+        self.frequencies = self.frequencies/number_characters
