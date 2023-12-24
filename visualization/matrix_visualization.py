8e7d32a54a1310174885b488f4584f1fe6b571da
llagonotte <lea.lagonotte@ensae.fr>
2023-12-24 14:22:28 +0000

    tesimage

--- visualization/matrix_visualization.py
+++ visualization/matrix_visualization.py
@@ -5,8 +5,7 @@ matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 
-
-language = "fr"
+"""language = "fr"
 matrix = np.loadtxt(f"/home/onyxia/work/projet_python_ds/training/Matrix/Matrix_{language}.txt")
 colors = np.zeros((len(matrix), len(matrix[0]), 3))
 for i in range(len(matrix)):
@@ -30,4 +29,55 @@ plt.xlabel('Lettre suivante')
 plt.ylabel('Lettre actuelle')
 plt.colorbar()  # Barre de couleur pour référence
 plt.show(block=True)
+plt.savefig(f"/home/onyxia/work/projet_python_ds/training/Matrix/Matrix_viz_{language}.png")
+"""
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')  # Use TkAgg for interactive zooming
+import matplotlib.pyplot as plt
+from matplotlib.widgets import RectangleSelector
+
+def onselect(eclick, erelease):
+    # eclick and erelease are the press and release events
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+
+    # Set the limits for zooming
+    ax.set_xlim(min(x1, x2), max(x1, x2))
+    ax.set_ylim(min(y1, y2), max(y1, y2))
+
+    # Redraw the plot with the zoomed region
+    plt.draw()
+
+language = "fr"
+matrix = np.loadtxt(f"/home/onyxia/work/projet_python_ds/training/Matrix/Matrix_{language}.txt")
+colors_hsv = np.zeros((255, 255, 3))
+
+for i in range(255):
+    for j in range(255):
+        alpha = 0.25
+        colors_hsv[i, j, 0] = matrix[i, j] ** alpha
+
+colors_rgb = plt.get_cmap('hsv')(colors_hsv[:, :, 0])
+
+# Create a figure and axis object
+fig, ax = plt.subplots(figsize=(8, 6))
+
+# Display the colors representing frequencies
+im = ax.imshow(colors_rgb, interpolation='nearest')
+
+# Set title and labels
+ax.set_title('Couleurs en fonction des fréquences')
+ax.set_xlabel('Lettre suivante')
+ax.set_ylabel('Lettre actuelle')
+
+# Add colorbar for reference
+cbar = plt.colorbar(im)
+
+# Enable zooming functionality
+rs = RectangleSelector(ax, onselect)
+
+# Display the plot
+plt.show()
 plt.savefig(f"/home/onyxia/work/projet_python_ds/training/Matrix/Matrix_viz_{language}.png")
