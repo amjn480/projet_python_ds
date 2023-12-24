f50fd54a7ad2b4caf0b2828b3a0f36ae468c2cc0
llagonotte <lea.lagonotte@ensae.fr>
2023-12-24 15:22:48 +0000

    test

--- visualization/matrix_visualization.py
+++ visualization/matrix_visualization.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 language = "fr"
 matrix = np.loadtxt(f"/home/onyxia/work/projet_python_ds/training/Matrix/Matrix_{language}.txt")
 submatrix= matrix[97:123, 97:123].copy()
+submatrix=(submatrix.astype('float'))**0.25
 """colors = np.zeros((len(matrix), len(matrix[0]), 3))
 for i in range(len(matrix)):
     for j in range(len(matrix)):
@@ -42,23 +43,14 @@ plt.colorbar() """
 
 
 
-colors = np.zeros((len(submatrix), len(submatrix[0]), 3))
-for i in range(len(submatrix)):
-    for j in range(len(submatrix)):
-        colors[i][j] = [submatrix[i][j], submatrix[i][j], submatrix[i][j]]
-
-colors_hsv = np.zeros((len(submatrix), len(submatrix), 3))  # Matrice pour stocker les couleurs en HSV
-for i in range(len(submatrix)):
-    for j in range(len(submatrix)):
-        alpha=0.25
-        colors_hsv[i, j, 0] = submatrix[i, j]**alpha  # Teinte correspondant à la fréquence
 
 # Conversion des couleurs de l'espace HSV à RGB pour l'affichage
 #colors_rgb = plt.get_cmap('hsv')(colors_hsv[:, :, 0])  # Utilisation uniquement de la teinte pour la colormap
 
 # Affichage des couleurs représentant les fréquences
 plt.figure(figsize=(8, 8))
-plt.imshow(colors_hsv, interpolation='nearest', cmap='Greys')
+plt.imshow(submatrix, interpolation='nearest', cmap='Greys')
+plt.axis('off')
 plt.title('Couleurs en fonction des fréquences')
 #plt.xlabel('Lettre suivante')
 #plt.ylabel('Lettre actuelle')
