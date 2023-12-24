b1bcf975bbb18f776725a784cf0ad903c781a654
llagonotte <lea.lagonotte@ensae.fr>
2023-12-24 15:48:20 +0000

    t

--- visualization/matrix_visualization.py
+++ visualization/matrix_visualization.py
@@ -21,7 +21,8 @@ cols_to_keep = np.array([32] + list(range(97, 123)) + list(range(224, 255)))
 
 # Use advanced indexing to keep the desired portion
 submatrix = matrix[np.ix_(rows_to_keep,cols_to_keep )] .copy()
-submatrix=(submatrix.astype('float'))
+submatrix=(submatrix.astype('float'))**0.25
+submatrix[submatrix>1]=1
 """colors = np.zeros((len(matrix), len(matrix[0]), 3))
 for i in range(len(matrix)):
     for j in range(len(matrix)):
