622183659b8168001fea79702da13da2bae4ef22
billy-ship-it <npeyrouset@gmail.com>
2023-12-26 17:48:22 +0000

    add frequencies visualization for the different languages

--- visualization/frequency_visualization/frequency_visualisation.py
+++ visualization/frequency_visualization/frequency_visualisation.py
@@ -2,32 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def matrix_visualiation(language):
-    matrix = np.loadtxt(f"/home/onyxia/work/projet_python_ds/training/Matrix/Matrix_{language}.txt")
-    colors = np.zeros((len(matrix), len(matrix[0]), 3))
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            colors[i][j] = [matrix[i][j], matrix[i][j], matrix[i][j]]
+def frequency_visualization(language):
+    frequencies = np.loadtxt(f"/home/onyxia/work/projet_python_ds/training/Frequency/Frequencies_{language}.txt")
+    colors = plt.cm.Blues(frequencies)
+    plt.figure(figsize=(255, 10))
+    for i, freq in enumerate(colors):
+        plt.fill_between([i, i + 1], 0, 1, color=freq)
+        plt.text(i + 0.5, 1.1, chr(i), ha='center', fontsize=8)
 
-    colors_hsv = np.zeros((255, 255, 3))  # Matrice pour stocker les couleurs en HSV
-    for i in range(255):
-        for j in range(255):
-            colors_hsv[i, j, 0] = matrix[i, j]  # Teinte correspondant à la fréquence
+    plt.title('Exemple de fréquences des caractères ASCII')
+    plt.xlabel('Caractères ASCII')
+    plt.ylabel('Fréquence')
 
-    # Conversion des couleurs de l'espace HSV à RGB pour l'affichage
-    colors_rgb = plt.get_cmap('Blues')(colors_hsv[:, :, 0])  # Utilisation uniquement de la teinte pour la colormap
+    plt.xlim(0, len(frequencies))
+    plt.ylim(0, 1)
+    plt.axis('off')  # Masquer les axes
 
-    # Affichage des couleurs représentant les fréquences
-    plt.figure(figsize=(8, 6))
-    plt.imshow(colors_rgb, interpolation='nearest')
-    plt.title('Couleurs en fonction des fréquences')
-    plt.xlabel('Lettre suivante')
-    plt.ylabel('Lettre actuelle')
-    plt.colorbar()  # Barre de couleur pour référence
     plt.show()
-    plt.savefig(f'/home/onyxia/work/projet_python_ds/visualization/matrix_visualization/matrix_{language}.png')
+    plt.savefig(f'/home/onyxia/work/projet_python_ds/visualization/frequency_visualization/frequencies_{language}.png')
 
 
 # To save the different matrix
 for language in ['fr', 'en', 'es', 'de', 'nl', 'it', 'af', 'ca', 'pl', 'sv']:
-    matrix_visualiation(language=language)
+    frequency_visualization(language=language)
