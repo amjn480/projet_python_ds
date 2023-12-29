5079f9946b2b3f78468630eba4dfdf5622a6bb2f
billy-ship-it <npeyrouset@gmail.com>
2023-12-29 18:29:11 +0000

    add path

--- count_letters.py
+++ count_letters.py
@@ -1,12 +1,15 @@
 import os
 import json
 
+path_script = os.path.abspath(__file__)
+path_root = os.path.dirname(path_script)
+
 language_list = ["en", "pl", "fr", "es", "it", "de", "ca", "sv", "af", "nl"]
 for elt in language_list:
-    path_list = os.listdir(f"/home/onyxia/work/projet_python_ds/data/{elt}")
+    path_list = os.listdir(path_root + f"/data/{elt}")
     total_char = 0
     for file in path_list:
-        with open(f"/home/onyxia/work/projet_python_ds/data/{elt}/" + file, 'r', encoding='utf-8') as article:
+        with open(path_root + f"/data/{elt}/" + file, 'r', encoding='utf-8') as article:
             try:
                 article = json.load(article)
                 for word in article:
