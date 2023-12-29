c48cf096385896ff865242e9fa5310c0f76d04cd
billy-ship-it <npeyrouset@gmail.com>
2023-12-29 10:28:59 +0000

    add changes file frequency_visualization

--- visualization/frequency_visualization/frequency_visualisation.py
+++ visualization/frequency_visualization/frequency_visualisation.py
@@ -8,7 +8,7 @@ def frequency_visualization(language):
     # Loading of the file
     frequencies = np.loadtxt(f"/home/onyxia/work/projet_python_ds/training/Frequency/Frequencies_{language}.txt")
     colors = plt.cm.Blues(frequencies)
-    plt.figure(figsize=(255, 10))
+    plt.figure(figsize=(256, 10))
     for i, freq in enumerate(colors):
         plt.fill_between([i, i + 1], 0, 1, color=freq)
         plt.text(i + 0.5, 1.1, chr(i), ha='center', fontsize=8)
@@ -29,7 +29,7 @@ def worcloud_visualization(language):
     """Provide another visualization of frequencies."""
     # Load of the frequencies
     frequencies = np.loadtxt(f"/home/onyxia/work/projet_python_ds/training/Frequency/Frequencies_{language}.txt")
-    freq = {chr(n): frequencies[n] for n in range(255)}
+    freq = {chr(n): frequencies[n] for n in range(256)}
     wordcloud = WordCloud(width=800, height=400, background_color='white', prefer_horizontal=1.0).generate_from_frequencies(freq)
     plt.figure(figsize=(10, 8))
     plt.imshow(wordcloud, interpolation='bilinear')
@@ -41,7 +41,7 @@ def worcloud_visualization(language):
 def frequency_visualization_zoom(language):
     """Provide a zoom of the first visualization of frequencies."""
     frequencies = np.loadtxt(f"/home/onyxia/work/projet_python_ds/training/Frequency/Frequencies_{language}.txt")
-    frequencies=frequencies[np.ix_([32] + list(range(97, 123)) + list(range(224, 255)))]
+    frequencies = frequencies[np.ix_([32] + list(range(97, 123)) + list(range(224, 255)))]
     colors = plt.cm.Blues(frequencies)
     plt.figure(figsize=(58, 5))
     for i, freq in enumerate(colors):
@@ -59,8 +59,8 @@ def frequency_visualization_zoom(language):
     plt.savefig(f'/home/onyxia/work/projet_python_ds/visualization/frequency_visualization/frequencies_zoom_{language}.png')
 
 
-# To save the different matrix
+# To save the graphics
 for language in ['fr', 'en', 'es', 'de', 'nl', 'it', 'af', 'ca', 'pl', 'sv']:
-    #frequency_visualization(language=language)
-    #worcloud_visualization(language=language)
+    # frequency_visualization(language=language)
+    # worcloud_visualization(language=language)
     frequency_visualization_zoom(language=language)
